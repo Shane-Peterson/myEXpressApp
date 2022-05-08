175062eb30ddcc6ccd5d0736c57b1cd5f7b4cea8
Shane-Peterson <liuzhelei.hwc@gmail.com>
2022-05-08 17:06:36 +0800

    update main.

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* NOTE:GET users listing. */
+/* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 
